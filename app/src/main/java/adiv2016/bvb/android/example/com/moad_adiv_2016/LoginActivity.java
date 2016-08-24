b8721a2949702a03660f905739bfb0999116fb32
maheshsp123 <maheshsp123@gmail.com>
2016-08-24 14:00:01 +0530

    commit 3 24/8/2016 1:42pm

--- app/src/main/java/adiv2016/bvb/android/example/com/moad_adiv_2016/LoginActivity.java
+++ app/src/main/java/adiv2016/bvb/android/example/com/moad_adiv_2016/LoginActivity.java
@@ -2,12 +2,21 @@ package adiv2016.bvb.android.example.com.moad_adiv_2016;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText userNameET_ref;
+    EditText passwdET_ref;
+    Button loginSubmitBtn_ref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        userNameET_ref=(EditText) findViewById(R.id.userNameET_Id);
+
+
     }
 }
