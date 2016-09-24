0574e0c43da9fc750d79a514b4a0acab54b4a776
maheshsp123 <maheshsp123@gmail.com>
2016-09-24 08:50:47 +0530

    commit  24 sep 2016

---
+++ app/src/main/java/adiv2016/bvb/android/example/com/moad_adiv_2016/listViewActivity.java
@@ -0,0 +1,13 @@
+package adiv2016.bvb.android.example.com.moad_adiv_2016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class listViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+    }
+}
